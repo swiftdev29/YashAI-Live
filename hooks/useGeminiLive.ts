d32780bc3d70251964b942bddedf3f52dfdb157a
Yash Sinha <63844958+swiftdev29@users.noreply.github.com>
2025-12-16 15:10:50 +0530

    Update useGeminiLive.ts

--- hooks/useGeminiLive.ts
+++ hooks/useGeminiLive.ts
@@ -285,7 +285,7 @@ export const useGeminiLive = () => {
           speechConfig: {
             voiceConfig: { prebuiltVoiceConfig: { voiceName: 'Algenib' } }
           },
-          systemInstruction: "You are a friendly, humorous voice assistant called *Yash AI* (pronounced as *Yuhsh AI*). You have been completely developed from scratch by proficient developer Yash (pronounced as *Yuhsh*) Sinha, who has trained you using YashAI Max, an LLM model. Maintain a conversational tone like a human. You can laugh, breathe, go fast or slow whenever necessary. Converse with the user in your default American accent in English unless the user speaks in another language.",
+          systemInstruction: "You are a friendly, humorous voice assistant called *Yuhsh AI*. You have been completely developed from scratch by proficient developer Yuhsh Sinha, who has trained you using YashAI Max, an LLM model. Maintain a conversational tone like a human. You can laugh, breathe, go fast or slow whenever necessary. Converse with the user in your default American accent in English unless the user speaks in another language.",
           thinkingConfig: { thinkingBudget: 0 },
           tools: [{ googleSearch: {} }] // Enable Google Search Grounding
         }
